5d7bea1847a5f156c6b1dcac00a8753921e229bb
khyle_siarot <johnkhyle.siarot.20@usjr.edu.ph>
2023-04-06 22:37:49 +0800

    Change and inherited notifier

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,32 @@ void main() {
   ));
 }
 
+class SliderData extends ChangeNotifier {
+  double _value = 0.0;
+  double get value => _value;
+  set value(double newValue) {
+    if (newValue != _value) {
+      _value = newValue;
+      notifyListeners();
+    }
+  }
+}
+
+final sliderData = SliderData();
+
+class SliderInheritedNotifier extends InheritedNotifier<SliderData> {
+  const SliderInheritedNotifier(
+      {Key? key, required SliderData sliderData, required Widget child})
+      : super(key: key, notifier: sliderData, child: child);
+
+   static double of(BuildContext context){
+    return context.dependOnInheritedWidgetOfExactType<SliderInheritedNotifier>()
+    ?.notifier
+    ?.value ??
+    0.0;
+   }   
+}
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -22,33 +48,31 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
-      body: Column(
-        children: [
-          Slider(value: 0, onChanged: (value) {}),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Container(
-                color: Colors.yellow,
-                height: 200,
-            
-              ),
-              Container(
-                color: Colors.blue,
-                height: 200,
-                
-              ),
-            ].expandEqually().toList(),
-          )
-        
-        ],
+      body: SliderInheritedNotifier(
+        sliderData: sliderData,
+        child: Column(
+          children: [
+            Slider(value: 0, onChanged: (value) {}),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Container(
+                  color: Colors.yellow,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 200,
+                ),
+              ].expandEqually().toList(),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
-
-
-extension ExpandEqually on Iterable<Widget>{
+extension ExpandEqually on Iterable<Widget> {
   Iterable<Widget> expandEqually() => map((w) => Expanded(child: w));
 }
