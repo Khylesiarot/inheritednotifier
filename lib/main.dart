bf89819d8be23bf4c73c05c7a98284e7fb59408e
khyle_siarot <johnkhyle.siarot.20@usjr.edu.ph>
2023-04-06 22:25:12 +0800

    Slider & Color Boxes

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,54 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-    ));
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+  ));
 }
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
-);
+      body: Column(
+        children: [
+          Slider(value: 0, onChanged: (value) {}),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                color: Colors.yellow,
+                height: 200,
+            
+              ),
+              Container(
+                color: Colors.blue,
+                height: 200,
+                
+              ),
+            ].expandEqually().toList(),
+          )
+        
+        ],
+      ),
+    );
   }
+}
+
+
+
+extension ExpandEqually on Iterable<Widget>{
+  Iterable<Widget> expandEqually() => map((w) => Expanded(child: w));
 }
